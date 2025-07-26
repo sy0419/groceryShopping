a7759c002f4a777f5985f7ac3f29801789c32272
sy0419 <psy041912@gmail.com>
2025-07-26 20:57:26 +0900

    Connect with database.

--- shoppingcart/src/main/java/com/example/repository/ProductRepository.java
+++ shoppingcart/src/main/java/com/example/repository/ProductRepository.java
@@ -1,5 +1,16 @@
 package com.example.repository;
 
-public class ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.model.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
     
 }
+
+// Item repository where connects with database.
+// Entity of Product restore or inquires or delects from database.
+// Spring automatically makes the fucation and offer to use them.
+// DB와 연결된 상품 저장소
+// Product 엔티티를 DB에 저장하거나 조회하거나 삭제할 수 있게 해줍니다.
+// 직접 구현 안 해도 Spring이 자동으로 기능을 제공합니다.
