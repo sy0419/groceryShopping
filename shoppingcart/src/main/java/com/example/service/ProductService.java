15aed9c85216df02fa9a44c57f3a716c287723fe
sy0419 <psy041912@gmail.com>
2025-07-27 01:35:40 +0900

    Run the works which user requests.

--- shoppingcart/src/main/java/com/example/service/ProductService.java
+++ shoppingcart/src/main/java/com/example/service/ProductService.java
@@ -1,5 +1,32 @@
 package com.example.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.model.Product;
+import com.example.repository.ProductRepository;
+
+
+@Service
 public class ProductService {
-    
+    private ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public void saveProduct(Product product) {
+        productRepository.save(product);
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
 }
+
+// 
