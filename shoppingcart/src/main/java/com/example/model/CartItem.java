3daf8290918d8b3f2c9a83beffa875c388e01a03
sy0419 <psy041912@gmail.com>
2025-07-26 20:45:33 +0900

    Make a list of item(s) in the cart.

--- shoppingcart/src/main/java/com/example/model/CartItem.java
+++ shoppingcart/src/main/java/com/example/model/CartItem.java
@@ -39,3 +39,10 @@ public class CartItem{
         this.quantity = quantity;
     }
 }
+
+// The items which is in the cart by customer
+// This code shows the structure when user put the item into the cart.
+// Based on Product, this code also shows how many of the items into the cart.
+// 장바구니에 담긴 상품 항목 (Entity)
+// 사용자가 장바구니에 상품을 담았을 때의 구조를 나타냅니다.
+// 즉, Product를 참조하면서 "이걸 몇 개 담았는지"를 함께 저장합니다.
