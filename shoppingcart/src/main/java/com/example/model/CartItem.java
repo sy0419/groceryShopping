cdaa9dcdb362871bec6bbfb3feebf23c32d05f0e
sy0419 <psy041912@gmail.com>
2025-07-26 17:32:16 +0900

    Make a list of item in the cart.

--- shoppingcart/src/main/java/com/example/model/CartItem.java
+++ shoppingcart/src/main/java/com/example/model/CartItem.java
@@ -1,6 +1,7 @@
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 
 @Entity
@@ -11,4 +12,28 @@ public class CartItem{
     @ManyToOne
     private Product product;
     private int quantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
