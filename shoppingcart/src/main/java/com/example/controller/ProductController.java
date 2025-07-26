e69461b9178ec34414d855dac79de241ea008d61
sy0419 <psy041912@gmail.com>
2025-07-27 02:12:30 +0900

    Makes the works running properly.

--- shoppingcart/src/main/java/com/example/controller/ProductController.java
+++ shoppingcart/src/main/java/com/example/controller/ProductController.java
@@ -1,5 +1,48 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.Product;
+import com.example.service.CartService;
+import com.example.service.ProductService;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
-    
+    private final ProductService productService;
+    private final CartService cartService;
+
+    public ProductController(ProductService productService, CartService cartService) {
+        this.productService = productService;
+        this.cartService = cartService;
+    }
+
+    @PostMapping
+    public void saveProduct(@RequestBody Product product) {
+        productService.saveProduct(product);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        productService.deleteProductById(id);
+    }
+
+    @PatchMapping("/{id}")
+    public void updateQuantity(@PathVariable Long id, @RequestBody int quantity) {
+        cartService.updateQuantity(id, quantity);
+    }
 }
